7e3ed12e67c6a94c49e9d9293565667f8e13f90b
Batcks <sntihl07@gmail.com>
2026-08-01 19:30:06 +0200

    volvemos a v1.3

--- main.py
+++ main.py
@@ -58,8 +58,7 @@ async def obtener_catalogo_playwright():
         page = await browser.new_page()
         
         await page.goto(URL_GAME)
-        await page.wait_for_selector('.figure', state='attached', timeout=20000)
-        await page.wait_for_timeout(5000)
+        await page.wait_for_selector('.figure', timeout=15000)
         
         # --- LÓGICA DE SCROLL MEJORADA ---
         intentos_vacios = 0
